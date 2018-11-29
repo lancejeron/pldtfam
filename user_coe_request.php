3608b55efab60428fd3b08e1972eb312bd174a59
robertojoaquin <joaqszxc@gmail.com>
2018-11-29 11:08:08 -1100

    Create user_coe_request.php

---
+++ user_coe_request.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Walk in Registration</title>
+
+    <!-- Bootstrap -->
+    <link href="../vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="../vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <!-- NProgress -->
+    <link href="../vendors/nprogress/nprogress.css" rel="stylesheet">
+    
+    <!-- Custom Theme Style -->
+    <link href="../build/css/custom.min.css" rel="stylesheet">
+  </head>
+
+  <body class="nav-md">
+    <div class="container body">
+      <div class="main_container">
+        <!-- page content -->
+        <div class="right_col" role="main">
+          <div class="">
+            <div class="page-title">
+              <div class="title_left">
+                <h3>Request for Certificate of Employment</h3>
+              </div>
+
+
+            </div>
+            <div class="clearfix"></div>
+
+            <div class="row">
+              <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2>Request for Certificate of Emplyment</h2>
+             
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
+
+                    <form class="form-horizontal form-label-left" novalidate>
+                      <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="date_prepared">Date Prepared</label>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                          <input type="text" id="first-name" class="form-control col-md-7 col-xs-12">
+                        </div>
+                      </div>
+        
+                      <div class="form-group">
+                          <label class="control-label col-md-3 col-sm-3 col-xs-12" for="emp_no">Employee Number</label>
+                          <div class="col-md-6 col-sm-6 col-xs-12">
+                            <input type="number" id="emp_no" name="emp_no" class="form-control col-md-7 col-xs-12">
+                          </div>
+                      </div>
+        
+                      <div class="form-group">
+                          <label for="name" class="control-label col-md-3 col-sm-3 col-xs-12">Employee Name</label>
+                          <div class="col-md-6 col-sm-6 col-xs-12">
+                            <input id="name" class="form-control col-md-7 col-xs-12" type="text" name="name">
+                          </div>
+                      </div>
+                      
+                      <div class="form-group">
+                          <label for="division" class="control-label col-md-3 col-sm-3 col-xs-12">Division</label>
+                          <div class="col-md-6 col-sm-6 col-xs-12">
+                            <input id="division" class="form-control col-md-7 col-xs-12" type="text" name="division">
+                          </div>
+                      </div>
+        
+                      <div class="form-group">
+                          <label for="tel_no" class="control-label col-md-3 col-sm-3 col-xs-12">Telephone Number</label>
+                          <div class="col-md-6 col-sm-6 col-xs-12">
+                            <input id="tel_no" class="form-control col-md-7 col-xs-12" type="number" name="tel_no">
+                          </div>
+                      </div>
+                      
+                      <div class="col-md-8 center-margin">
+                          <div class="form-group">
+                              <h4>Select type of certificate:</h4>
+                                  <div class="form-group">
+                                      <div class="radio">
+                                          <label>
+                                            <input type="radio" checked="" value="option1" id="optionsRadios1" name="optionsRadios">Certificate of Employment
+                                          </label>
+                                      </div>
+                                      <div class="radio">
+                                          <label>
+                                            <input type="radio" value="option2" id="optionsRadios2" name="optionsRadios">Certificate of Employment and Compensation
+                                          </label>
+                                      </div>
+                                  </div>
+                          </div>  
+                          
+                        <div class="form-group">
+                            <h4>Select purpose:</h4>
+                            <div class="row">
+                                <div class='col-sm-4'>
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox" value="">Car Loan
+                                        </label>
+                                    </div>
+        
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox" value="">Loan Application 
+                                        </label>
+                                    </div>
+        
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox" value="">Housing Loan (Bank/HDMF0) 
+                                        </label>
+                                    </div>
+        
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox" value="">Credit Card
+                                        </label>
+                                    </div>
+                                    
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox" value="">Cellphone Application
+                                        </label>
+                                    </div>
+        
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox" value="">Firearms License
+                                        </label>
+                                    </div>
+        
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox" value="">School
+                                        </label>
+                                    </div>
+                                </div>    
+                            
+                                <div class='col-sm-4'>
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox" value="">Visa/Travel Abroad
+                                        </label>
+                                    </div>
+                                    
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox" value="">Migration
+                                        </label>
+                                    </div>
+                                        
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox" value="">Employment (local/abroad)
+                                        </label>
+                                    </div>
+        
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox" value="">HDMF (Loan/Maturity)
+                                        </label>
+                                    </div>
+        
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox" value="">SSS Claim/Pension
+                                        </label>
+                                    </div>
+                                    
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox" value="">PhilHealth
+                                        </label>
+                                    </div>
+                                        
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox" value="">Reference/Others
+                                        </label>
+                                    </div>
+                                </div>    
+                            </div>                                       
+                        </div>
+                      </div>
+                    <div class="form-group">
+                        <label for="claimersname" class="control-label col-md-3 col-sm-3 col-xs-12">Received by</label>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                        <input id="claimersname" class="form-control col-md-7 col-xs-12" type="text" name="claimersname">
+                        </div>
+                    </div>
+                    
+                    <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="claimdate">Claim Date</label>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                        <input type="claimdate" id="claimdate" class="form-control col-md-7 col-xs-12">
+                        </div>
+                    </div>
+                    
+                    <div class="ln_solid"></div>
+                    
+                    <div class="form-group">
+                        <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+                          <button class="btn btn-primary" type="button">Cancel</button>
+                          <button class="btn btn-primary" type="reset">Reset</button>
+                          <button type="submit" class="btn btn-success">Submit</button>
+                        </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- /page content -->
+
+        <!-- footer content -->
+        <footer>
+          <div class="pull-right">
+            Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+          </div>
+          <div class="clearfix"></div>
+        </footer>
+        <!-- /footer content -->
+      </div>
+    </div>
+
+    <!-- jQuery -->
+    <script src="../vendors/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="../vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- FastClick -->
+    <script src="../vendors/fastclick/lib/fastclick.js"></script>
+    <!-- NProgress -->
+    <script src="../vendors/nprogress/nprogress.js"></script>
+    <!-- validator -->
+    <script src="../vendors/validator/validator.js"></script>
+
+    <!-- Custom Theme Scripts -->
+    <script src="../build/js/custom.min.js"></script>
+	
+  </body>
+</html>
