71831058aa3f9adb09e073ee4d3d0595d890b09b
Lance Jeron San Pablo <lancejeron26@gmail.com>
2018-12-04 10:14:28 +0800

    swalla

--- production/user_coe_request_insert_record.php
+++ production/user_coe_request_insert_record.php
@@ -19,11 +19,17 @@
     $insert_sql="INSERT INTO view_coe_request VALUES ('$start_time', '', '$emp_name', '$type_of_coe', '$purpose', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '$emp_id')";
     if (!mysqli_query($conn, $insert_sql)) {
 
-        echo("Error description: " . mysqli_error($conn)); 
+        // echo("Error description: " . mysqli_error($conn));
+        // echo("invalid");
+        // $response_array['status'] = 'invalid';
+        // header("refresh:5; url=user_coe_request.php");
     }
     else{
-        echo "Request Submitted! Please inform the staff that you had successfully submitted your request." . "<br>";
+        // echo "Request Submitted! Please inform the staff that you had successfully submitted your request." . "<br>";
+        // echo("success");
+        // $response_array['status'] = 'success'; 
+        // header("refresh:5; url=user_coe_request.php");
     }
-    header("refresh:5; url=user_coe_request.php");
+    // header("refresh:5; url=user_coe_request.php");
 
 ?>
