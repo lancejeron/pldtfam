8ceb5e4ae4108bdc9dc61c03bc62721bce9c0d3e
robertojoaquin <joaqszxc@gmail.com>
2018-12-04 16:32:47 -1100

    purpose

--- production/maintenance_purpose.php
+++ production/maintenance_purpose.php
@@ -15,7 +15,7 @@
 
 		$sql = 'SELECT *, DATE_FORMAT(start_time, "%M %e, %Y @ %r") AS start_time2 FROM view_coe_request AS tbl1
 				WHERE NOT EXISTS
-					(SELECT * FROM prepared_certificates as tbl2
+					(SELECT * FROM tblmpurpose as tbl2
 					WHERE tbl2.emp_id = tbl1.emp_id AND tbl2.date_prepared=tbl1.start_time)';
 		$result = mysqli_query($conn, $sql);
 		if (!$result) {
@@ -85,7 +85,7 @@
               <div class="menu_section">
                 <h3>Menu</h3>
                 <ul class="nav side-menu">
-                  <li><a><i class="fa fa-edit"></i> Certificate of Employment <span class="fa fa-chevron-down"></span></a>
+                  <li><a><i class="fa fa-edit"></i> COE <span class="fa fa-chevron-down"></span></a>
                     <ul class="nav child_menu">
                       <li><a href="index.php">Requests</a></li>
                       <li><a href="index3.php">Finished</a></li>
@@ -128,7 +128,7 @@
           <div class="">
             <div class="page-title">
               <div class="title_left">
-                <h3>Certificate of Employment</h3>
+                <h3>Maintenance</h3>
               </div>
 
             </div>
@@ -139,7 +139,7 @@
               <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="x_panel">
                   <div class="x_title">
-                    <h2>Requests</h2>
+                    <h2>Purpose</h2>
                     <!-- <button type='button' class='btn btn-success pull-right btn-md' data-toggle='modal' data-target='#walkin'><i class="fa fa-plus"></i> Create Request</button> -->
                     <div class="clearfix"></div>
                   </div>
@@ -148,22 +148,9 @@
                       <table id='mydatatable' class='table table-striped table-bordered'>
                         <thead>
                           <tr>
-                            <th>Start Time</th>
-                            <th>Employee ID</th>
-                            <th>Email</th>
-                            <th>Employee Name</th>
-                            <th>Type of COE</th>
-                            <th>Purpose</th>
-                            <th>Salary</th>
-                            <th>Question</th>
-                            <th>Statement</th>
-                            <th>Request for</th>
-                            <th>Request for Name</th>
-                            <th>Positon Title</th>
-                            <th>Persno</th>
-                            <th>MMProv</th>
-                            <th>Other Instruction</th>
-                            <!-- <th>Action</th> -->
+                            <th>Name</th>
+                            <th>Status</th>
+                            <th>Action</th>
                           </tr>
                         </thead>
                         <?php
@@ -171,22 +158,7 @@
                             echo '
                             <tr>
                             <td>' . $row["start_time"] . '</td>
-                            <td>' . $row["emp_id"] . '</td>
-                            <td>' . $row["email"] . '</td>
-                            <td>' . $row["emp_name"] . '</td>
-                            <td>' . $row["type_of_coe"] . '</td>
-                            <td>' . $row["purpose"] . '</td>
-                            <td>' . $row["_salary"] . '</td>
-                            <td>' . $row["question1"] . '</td>
-                            <td>' . $row["statement"] . '</td>
-                            <td>' . $row["reqt_for"] . '</td>
-                            <td>' . $row["reqt_for_name"] . '</td>
-                            <td>' . $row["position_title"] . '</td>
-                            <td>' . $row["persno"] . '</td>
-                            <td>' . $row["MMProv"] . '</td>
-                            <td>' . $row["other_instruction"] . '</td>
-
-                            
+                            <td>' . $row["emp_id"] . '</td>                           
                           </tr>
                           ';
                           }
@@ -199,126 +171,6 @@
             </div>
           </div>
         </div>
-<!-- MODAL -->
-<div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-hidden="true">
-	<div class="modal-dialog modal-lg">
-		<form id="demo-form2" data-parsley-validate class="form-horizontal form-label-left" method='POST' action='index_insert_record.php'>
-			<div class="modal-content">
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span>
-					</button>
-					<h4 class="modal-title" id="myModalLabel">Process Request</h4>
-				</div>
-				<div class="modal-body">
-					<div class="form-group">
-						<label class="control-label col-sm-2" for="ref_no">Reference Number</label>
-						<div class="col-sm-10">
-							<input type="text" class="form-control" id="ref_no" placeholder="" name="ref_no">
-						</div>
-					</div>
-					<div class="form-group">
-						<label class="control-label col-sm-2" for="start_time">Date Prepared</label>
-						<div class="col-sm-10">          
-							<input type="text" class="form-control" id="start_time2" placeholder=""disabled>
-							<input type="text" class="form-control" id="start_time" placeholder="" name="start_time" style='display: none;'>
-						</div>
-					</div>
-					<div class="form-group">
-						<label class="control-label col-sm-2" for="emp_id">Employee ID</label>
-						<div class="col-sm-10">          
-							<input type="number" class="form-control" id="emp_id" placeholder="" disabled>
-							<input type="text" class="form-control" id="emp_id" placeholder="" name="emp_id" style='display: none;'>
-						</div>
-					</div>
-					
-					
-					<div class="form-group">
-						<label class="control-label col-sm-2" for="name">Name</label>
-						<div class="col-sm-10">          
-							<input type="text" class="form-control" id="name" placeholder="" disabled>
-							<input type="text" class="form-control" id="name" placeholder="" name="name" style='display: none;'>
-						</div>
-					</div>
-					
-					<div class="form-group">
-						<label class="control-label col-sm-2" for="purpose">Purpose</label>
-						<div class="col-sm-10">          
-							<input type="text" class="form-control" id="purpose" placeholder="" disabled>
-							<input type="text" class="form-control" id="purpose" placeholder="" name="purpose" style='display: none;'>
-						</div>
-					</div>
-					
-					<div class="form-group">
-						<label class="control-label col-sm-2" for="accomp_code">Accomp Code</label>
-						<div class="col-sm-10">          
-							<input type="text" class="form-control" id="accomp_code" placeholder="" name="accomp_code" >
-						</div>
-					</div>
-					
-					<div class="form-group">
-						<label class="control-label col-sm-2" for="type_of_coe">CBO Type</label>
-						<div class="col-sm-10">          
-							<input type="text" class="form-control" id="type_of_coe" placeholder="" disabled>
-							<input type="text" class="form-control" id="type_of_coe" placeholder="" name="type_of_coe" style='display: none;'>
-						</div>
-					</div>
-					
-					<div class="form-group">
-						<label class="control-label col-sm-2" for="control_id">Control ID</label>
-						<div class="col-sm-10">          
-							<input type="control_id" class="form-control" id="control_id" placeholder="" name="control_id">
-						</div>
-					</div>
-
-					<div class="form-group">
-						<label class="control-label col-sm-2" for="personal">Personal</label>
-						<div class="col-sm-10">          
-							<input type="personal" class="form-control" id="personal" placeholder="" name="personal">
-						</div>
-					</div>
-					
-					<div class="form-group">
-						<label class="control-label col-sm-2" for="req_status">Status</label>
-						<div class="col-sm-10">          
-								<select name="req_status" class="form-control" >
-									<!-- <option id="req_status" label="" value=""></option> -->
-									<option value="Processed">Processed</option>
-									<!-- <option value="Claim">Claim</option>  
-									<option value="Mail">Mail</option> -->
-									<option value="Claimed">Claimed</option>
-									<option value="Mailed">Mailed</option>
-
-								</select>                
-						</div>
-					</div>
-					
-					<div class="form-group">
-						<label class="control-label col-sm-2" for="claimersname">Claimer's Name</label>
-						<div class="col-sm-10">          
-							<input type="text" class="form-control" id="claimersname" placeholder="" name="claimersname">
-						</div>
-					</div>
-
-					<div class="form-group">
-						<label class="control-label col-sm-2" for="claimdate">Claim Date</label>
-						<div class="col-sm-10">          
-							<input type="text" class="form-control" id="claimdate" placeholder="" disabled>
-							<input type="text" class="form-control" id='test1' name="claimdate" style='display: none;'>
-						</div>
-					</div>
-							
-					<div class="ln_solid"></div>
-								
-				</div>
-				<div class="modal-footer">
-					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-					<input type="submit" class="btn btn-success" value='Process' onclick='getdatetime();'>
-				</div>
-			</div>
-		</form>
-	</div>
-</div>
-
 
         <!-- /page content -->
 
@@ -409,30 +261,6 @@
 		modal.find('.modal-body #claimersname').val(claimersname)
 		modal.find('.modal-body #claimdate').val(claimdate)
 	});
-</script>
-<script>
-	function getdatetime(){
-		var today = new Date();         
-		var dd = today.getDate();
-		var mm = today.getMonth()+1; //January is 0!
-		var yyyy = today.getFullYear();
-		var hh = today.getHours();
-		var min = today.getMinutes();
-		var sec = today.getSeconds();
-		// var parsetoday = new Date(today)
-		
-		if(dd<10) {
-			dd = '0'+dd
-		} 
-
-		if(mm<10) {
-			mm = '0'+mm
-		}
-
-		today = yyyy + '-' + mm + '-' + dd + ' '+ hh + ':' + min +':' +sec;
-
-		document.getElementById("test1").value=today;
-	}
 </script>
 <?php
 	}
