6c7f64fe355a2b4d17e2931ef3d80007db3e8175
Lance Jeron San Pablo <lancejeron26@gmail.com>
2018-12-05 10:50:23 +0800

    walk in changes

--- production/user_coe_request.php
+++ production/user_coe_request.php
@@ -5,7 +5,13 @@
 	$password = '';
 	$dbname = 'certificate';
 
-    $conn = mysqli_connect($servername, $username, $password, $dbname);		
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    $purpose_query = "SELECT * FROM tblmpurpose WHERE purpose_status IN ('active')";
+    $purpose_result = mysqli_query($conn, $purpose_query);
+		if (!$purpose_result) {
+			echo "Error:". mysqli_error($conn);
+		}	
 ?>
 
 <!DOCTYPE html>
@@ -76,113 +82,39 @@
                           <div class="form-group">
                               <h4>Select type of certificate:</h4>
                                   <div class="form-group">
+                                    <input type="text" id='typeofcoe' name='type_of_coe' style='display: none;'>
                                       <div class="radio">
                                           <label>
-                                            <input type="radio" id="optionsRadios1" name="type_of_coe" value='Cert of Employment' checked>Certificate of Employment
+                                            <input type="checkbox" class='typeofcoe' value='Cert of Employment'>Certificate of Employment
                                           </label>
                                       </div>
                                       <div class="radio">
                                           <label>
-                                            <input type="radio"  id="optionsRadios2" name="type_of_coe" value='Cert of Compensation'>Certificate of Employment and Compensation
+                                            <input type="checkbox" class='typeofcoe' value='Cert of Compensation'>Certificate of Employment and Compensation
                                           </label>
                                       </div>
                                   </div>
                           </div>  
                           
-                        <div class="form-group">
-                            <h4>Select purpose *:</h4>
-                            <input type="text" id="purpose" class='addpurpose' name='purpose' style='display: none;'>
-                            <!-- <textarea id='purpose' class='addpurpose' name='purpose'> 
-                            </textarea> -->
-                            <div class="row">
-                                <div class='col-sm-4'>
-                                    <div class="checkbox">
-                                        <label>
-                                            <input type="checkbox" class="checkbox2" id='smp1' value="Car Loan">Car Loan
-                                        </label>
-                                    </div>
-        
-                                    <div class="checkbox">
-                                        <label>
-                                            <input type="checkbox" class="checkbox2" value="Loan Application">Loan Application 
-                                        </label>
-                                    </div>
-        
-                                    <div class="checkbox">
-                                        <label>
-                                            <input type="checkbox" class="checkbox2" value="Housing Loan (Bank/HDMF0) ">Housing Loan (Bank/HDMF0) 
-                                        </label>
-                                    </div>
-        
-                                    <div class="checkbox">
-                                        <label>
-                                            <input type="checkbox" class="checkbox2" value="Credit Card">Credit Card
-                                        </label>
-                                    </div>
-                                    
-                                    <div class="checkbox">
-                                        <label>
-                                            <input type="checkbox" class="checkbox2" value="Cellphone Application">Cellphone Application
-                                        </label>
-                                    </div>
-        
-                                    <div class="checkbox">
-                                        <label>
-                                            <input type="checkbox" class="checkbox2" value="Firearms License">Firearms License
-                                        </label>
-                                    </div>
-        
-                                    <div class="checkbox">
-                                        <label>
-                                            <input type="checkbox" class="checkbox2" value="School">School
-                                        </label>
-                                    </div>
+                        <h4>Select purpose *:</h4>
+                        <div class="form-group" style='height: 200px; width: auto; overflow-y: scroll;'>
+                            <input type="text" id="purpose" class='addpurpose' name='purpose'  style='display: none;'>
+                            <!-- <div class="row"> -->
+                                <div class='col-sm-8'>
+                                    <?php
+                                        while($row = mysqli_fetch_array($purpose_result)){
+                                            echo '
+                                                <div class="checkbox">
+                                                    <label>
+                                                        <input type="checkbox" class="checkbox2" value="'. $row["purpose_name"] .'" >'. $row["purpose_name"] . '
+                                                    </label>
+                                                </div>
+                                            ';
+
+                                        }
+                                    ?>
                                 </div>    
-                            
-                                <div class='col-sm-4'>
-                                    <div class="checkbox">
-                                        <label>
-                                            <input type="checkbox" class="checkbox2" value="Visa/Travel Abroad">Visa/Travel Abroad
-                                        </label>
-                                    </div>
-                                    
-                                    <div class="checkbox">
-                                        <label>
-                                            <input type="checkbox" class="checkbox2" value="Migration">Migration
-                                        </label>
-                                    </div>
-                                        
-                                    <div class="checkbox">
-                                        <label>
-                                            <input type="checkbox" class="checkbox2" value="Employment (local/abroad)">Employment (local/abroad)
-                                        </label>
-                                    </div>
-        
-                                    <div class="checkbox">
-                                        <label>
-                                            <input type="checkbox" class="checkbox2" value="HDMF (Loan/Maturity)">HDMF (Loan/Maturity)
-                                        </label>
-                                    </div>
-        
-                                    <div class="checkbox">
-                                        <label>
-                                            <input type="checkbox" class="checkbox2" value="SSS Claim/Pension">SSS Claim/Pension
-                                        </label>
-                                    </div>
-                                    
-                                    <div class="checkbox">
-                                        <label>
-                                            <input type="checkbox" class="checkbox2" value="PhilHealth">PhilHealth
-                                        </label>
-                                    </div>
-                                        
-                                    <div class="checkbox">
-                                        <label>
-                                            <input type="checkbox" class="checkbox2" value="Reference/Others">Reference/Others
-                                        </label>
-                                    </div>
-                                </div>    
-                            </div>                                       
+                            <!-- </div> -->
                         </div>
                       </div>
                     
@@ -257,129 +189,94 @@
 		document.getElementById("start_time").value=today;
 	}
 </script>
-<!-- <script>
-    $(document).ready(function () {
-        $('#req_coebt').click(function(e) {
-            checked = $("input[type=checkbox]:checked").length;
-            if(!checked) {
-                swal("You must pick at least one purpose.","","info");
-                return false;
-            }
-            else{
-                var emp_id = $('#emp_id').val();
-                var emp_name = $('#emp_name').val();
-                if(emp_id=='' || emp_name==''){
-                    swal("Please fill the required(*) fields.","","info");
-                    e.preventDefault();
-                }
-                else{
-                    var form = $('#req_coe');   
-                    swal({
-                        title: "Are you sure you want to submit your request?",
-                        text: "Clicking ok will certify that you fully reviewed your request.",
-                        icon: "warning",
-                        buttons: {
-                            cancel: true,
-                            ok: {
-                                text: "Ok",
-                                value: "willsubmit",
-                            }
-                        },
-                    })
-                    .then((willsubmit)=>{
-                        if (willsubmit){
-                            swal({
-                                title: "Request will be submitted!",
-                                text: "Please inform us that you already submitted your request. Thank you.",
-                                icon: "success",
-                                // showConfirmButton: true,
-                                buttons: false,
-                            });
-                            setTimeout( function () { 
-                                form.submit();
-                            }, 6000);
-
-                        }
-                        else{
-                        }
-                    });
-                    e.preventDefault();
-                }
-            }
-        });
-    });
-</script> -->
 <script>
     $(".checkbox2").change(function() {
         var newval = $(this).val();
         var currval = $('#purpose').val();
         if(!this.checked){
-            var removeval= $('#purpose').val().replace(''+newval+' ', '');
+            var removeval= $('#purpose').val().replace(''+newval+'; ', '');
             $('#purpose').val(removeval);
-            // alert(removeval);
         }
         else{
-            $('#purpose').val(currval + ''+ $(this).val() + ' ');
+            $('#purpose').val(currval + ''+ $(this).val() + '; ');
+        }
+    });
+    $(".typeofcoe").change(function() {
+        var newval = $(this).val();
+        var currval = $('#typeofcoe').val();
+        if(!this.checked){
+            var removeval= $('#typeofcoe').val().replace(''+newval+'; ', '');
+            $('#typeofcoe').val(removeval);
+        }
+        else{
+            $('#typeofcoe').val(currval + ''+ $(this).val() + '; ');
         }
     });
 </script>
 <script>
     $(document).ready(function () {
         $('#req_coebt').click(function(e) {
-            checked = $("input[type=checkbox]:checked").length;
+            checked = $(".checkbox2:checkbox:checked").length;
+            checked2 = $(".typeofcoe:checkbox:checked").length;
             if(!checked) {
                 swal("You must pick at least one purpose.","","info");
                 return false;
             }
             else{
-                var emp_id = $('#emp_id').val();
-                var emp_name = $('#emp_name').val();
-                if(emp_id=='' || emp_name==''){
-                    swal("Please fill the required(*) fields.","","info");
-                    e.preventDefault();
+                if(!checked2) {
+                    swal("You must pick at least one type of certificate.","","info");
+                    return false;
                 }
                 else{
-                    var form = $('#req_coe');   
-                    swal({
-                        title: "Are you sure you want to submit your request?",
-                        text: "Clicking ok will certify that you fully reviewed your request.",
-                        icon: "warning",
-                        buttons: {
-                            cancel: true,
-                            ok: {
-                                text: "Ok",
-                                value: "willsubmit",
-                            }
-                        },
-                    })
-                    .then((willsubmit)=>{
-                        if (willsubmit){
-                            $.ajax({
-                                url: 'user_coe_request_insert_record.php',
-                                method: 'POST',
-                                data: $('#req_coe').serialize(),
-                                success: function(data){
-                                    console.log(data);
-                                    swal({
-                                        title: "Request submitted!",
-                                        text: "Please inform us that you already submitted your request. Thank you.",
-                                        icon: "success",
-                                        // showConfirmButton: true,
-                                        buttons: false,
-                                    });
-                                    setTimeout( function () {
-                                        location.reload(); 
-                                    }, 6000);
-                                },
-                                error: function(data){
-                                    swal("Oops...", "Something went wrong :(", "error");
+                    var emp_id = $('#emp_id').val();
+                    var emp_name = $('#emp_name').val();
+                    if(emp_id=='' || emp_name==''){
+                        swal("Please fill the required(*) fields.","","info");
+                        e.preventDefault();
+                    }
+                    else{
+                        var form = $('#req_coe');   
+                        swal({
+                            title: "Are you sure you want to submit your request?",
+                            text: "Clicking ok will certify that you fully reviewed your request.",
+                            icon: "warning",
+                            buttons: {
+                                cancel: true,
+                                ok: {
+                                    text: "Ok",
+                                    value: "willsubmit",
                                 }
-                            });
-                        }
-                        else{
-                        }
-                    });
-                    e.preventDefault();
+                            },
+                        })
+                        .then((willsubmit)=>{
+                            if (willsubmit){
+                                $.ajax({
+                                    url: 'user_coe_request_insert_record.php',
+                                    method: 'POST',
+                                    data: $('#req_coe').serialize(),
+                                    success: function(data){
+                                        console.log(data);
+                                        swal({
+                                            title: "Request submitted!",
+                                            text: "Please inform us that you already submitted your request. Thank you.",
+                                            icon: "success",
+                                            // showConfirmButton: true,
+                                            buttons: false,
+                                        });
+                                        setTimeout( function () {
+                                            location.reload(); 
+                                        }, 6000);
+                                    },
+                                    error: function(data){
+                                        swal("Oops...", "Something went wrong :(", "error");
+                                    }
+                                });
+                            }
+                            else{
+                            }
+                        });
+                        e.preventDefault();
+                    }
                 }
             }
         });
