0e8ba4718820dd9b21638e4fc437e567cf5c6ef0
Lance Jeron San Pablo <lancejeron26@gmail.com>
2019-01-09 15:39:05 +0800

    pdf inc

---
+++ fpdf181/test/index.php
@@ -0,0 +1,97 @@
+<?php
+require('../fpdf.php');
+
+class PDF extends FPDF
+{
+// Page header
+function Header()
+{
+    // Logo
+    $this->Image('../pldt.png',10,6,60);
+    // Times bold 15
+    // $this->SetFont('Times','B',42);
+    // Move to the right
+    // $this->Cell(20);
+    // // Title
+    // $this->Cell(0,10,'PLDT',0,0,'L');
+    // // Line break
+    $this->Ln(20);
+}
+
+// Page footer
+function Footer()
+{
+    // Position at 1.5 cm from bottom
+    $this->SetY(-40);
+    // Times italic 8
+    $this->SetFont('Times','I',8);
+    $this->MultiCell(0,1,'*** Must be an orginal computer pirntout without erasures to be valid. *** ',0,'L');
+    $this->MultiCell(0,7,'*** For verifiaction pls. contact: (632) 584-0247/0247/0255/0261/0264/0355 or email us at HRISAdvisory@pldt.com.ph.*** ',0,'L');
+    $this->SetFont('Times','',12);
+    $this->MultiCell(0,10,'eHR-1123-18-94252-CEC',0,'L');
+    $this->SetFont('Times','',8);
+    $this->SetTextColor(255,0,0);
+    $this->MultiCell(0,10,'PLDT General Office P.O. Box 2148 Makati City, Philippines',0,'R');
+    $this->SetFont('Times','B',8);
+    $this->MultiCell(0,5,'PLD 1',0,'R');
+
+
+}
+}
+
+$pdf = new PDF();
+// $pdf->AliasNbPages();
+$pdf->AddPage();
+$pdf->SetMargins(20,5);
+$pdf->Image('../pldt2.png',50,20,300);
+$pdf->SetFont('Times','B',14);
+$pdf->MultiCell(0,10,'CERTFICATE OF EMPLOYMENT AND COMPENSATION',0, 'C');
+$pdf->SetFont('','',12);
+$pdf->Ln(5);
+$pdf->MultiCell(0,5,'Date: November 12, 2019',0,'R');
+$pdf->Ln(5);
+$pdf->MultiCell(0,5,'         This is to certify that LANCE SAN PABLO is a regular employee of PLDT Inc. (formerly "Philippine Long Distance Telephone Company") since November 1, 1994 and presently holds the positon of Sr Telecom Associate.',0,'L');
+$pdf->Ln(5);
+$pdf->MultiCell(0,5,'         His present basic monthly salary is P100,000.00.',0,'L');
+$pdf->Ln(5);
+$pdf->MultiCell(0,5,'         Further, he recieved the following bonuses, premiums and allowances during the twelve-month period:',0,'L');
+$pdf->Ln(10);
+$pdf->MultiCell(0,5,'                     Mid-Year Bonus',0,'L');
+$pdf->MultiCell(0,5,'                     Christmas Bonus/13th Month Pay',0,'L');
+$pdf->MultiCell(0,5,'                     Unused Sick Leave Pay',0,'L');
+$pdf->MultiCell(0,5,'                     Other Earnings  ',0,'L');
+$pdf->MultiCell(0,5,'                     Other Bonuses  ',0,'L');
+$pdf->MultiCell(0,1,'                                                                                                          ____________________',0,'L');
+$pdf->MultiCell(0,5,'                     Total Other Income',0,'L');
+$pdf->Ln(10);
+$pdf->MultiCell(0,5,'         This cetification is being issued upon employee\'s request for Visa Application. Visa Application. Visa Application. Visa Application. Visa Application. Visa Application. Visa Application. Visa Application.',0,'L');
+$pdf->Ln(30);
+// $pdf->MultiCell(0,5,'Renelia L. Villanueva',0,'R');
+// $pdf->MultiCell(0,5,'Head',0,'R');
+// $pdf->MultiCell(0,5,'HRIS & Automation',0,'R');
+
+
+// eto yung last wag dugtungan. sa taas ka magdagdag.
+$pdf->SetXY(137,107.5);
+$pdf->MultiCell(40,0,'999,999,999.00',0,'R');
+$pdf->SetXY(137,112.5);
+$pdf->MultiCell(40,0,'9,999,999.00',0,'R');
+$pdf->SetXY(137,117.5);
+$pdf->MultiCell(40,0,'999,999.00',0,'R');
+$pdf->SetXY(137,122.5);
+$pdf->MultiCell(40,0,'9,999.00',0,'R');
+$pdf->SetXY(137,127.5);
+$pdf->MultiCell(40,0,'999.00',0,'R');
+$pdf->SetXY(137,135);
+$pdf->MultiCell(40,0,'999,999,999,999.00',0,'R');
+
+// head
+$pdf->SetXY(111,194);
+$pdf->MultiCell(100,0,'Renelia L. Villanueva',0,'C');
+$pdf->SetXY(111,196);
+$pdf->MultiCell(100,5,'Head',0,'C');
+$pdf->SetXY(111,200);
+$pdf->MultiCell(100,5,'HRIS & Automation',0,'C');
+
+$pdf->Output();
+?>
---
+++ fpdf181/test/index2.php
@@ -0,0 +1,130 @@
+<?php
+require('../fpdf.php');
+
+class PDF extends FPDF
+{
+// Page header
+function Header()
+{
+    // Logo
+    $this->Image('../pldt.png',10,6,60);
+    // Times bold 15
+    // $this->SetFont('Times','B',42);
+    // Move to the right
+    // $this->Cell(20);
+    // // Title
+    // $this->Cell(0,10,'PLDT',0,0,'L');
+    // // Line break
+    $this->Ln(20);
+}
+
+// Page footer
+function Footer()
+{
+    // Position at 1.5 cm from bottom
+    $this->SetY(-45);
+    // Times italic 8
+    $this->SetFont('Times','I',8);
+    $this->MultiCell(0,1,'*** Must be an orginal computer pirntout without erasures to be valid. *** ',0,'L');
+    $this->MultiCell(0,7,'*** For verifiaction pls. contact: (632) 584-0247/0247/0255/0261/0264/0355 or email us at HRISAdvisory@pldt.com.ph.*** ',0,'L');
+  
+}
+}
+
+$pdf = new PDF('P', 'mm', 'Letter');
+// $pdf->AliasNbPages();
+$pdf->AddPage();
+$pdf->SetMargins(30,5);
+$pdf->Image('../pldt2.png',50,20,300);
+$pdf->SetFont('Times','B',14);
+// $pdf->MultiCell(0,10,'CERTFICATE OF EMPLOYMENT AND COMPENSATION',0, 'C');
+$pdf->SetFont('','',12);
+$pdf->Ln(5);
+$pdf->MultiCell(0,5,' November 12, 2019',0,'R');
+$pdf->Ln(5);
+$pdf->MultiCell(0,5,' eHr-1212-1894533-CEC',0,'R');
+$pdf->Ln(20);
+$pdf->SetFont('Times','B',12);
+$pdf->MultiCell(0,5,'TO WHOM IT MAY CONCERN:',0,'L');
+$pdf->SetFont('','',12);
+$pdf->Ln(10);
+$pdf->MultiCell(0,5,'         This is to certify that the following data are true and indicated in the records of this company:',0,'L');
+$pdf->Ln(10);
+$pdf->SetFont('Times','B',12);
+$pdf->MultiCell(0,5,'ID NUMBER:',0,'L');
+$pdf->Ln(5);
+$pdf->MultiCell(0,5,'NAME OF EMPLOYEE:',0,'L');
+$pdf->Ln(5);
+$pdf->MultiCell(0,5,'DATE EMPLOYED:',0,'L');
+$pdf->Ln(5);
+$pdf->MultiCell(0,5,'DATE SEPARATED:',0,'L');
+$pdf->Ln(5);
+$pdf->MultiCell(0,5,'DESIGNATION:',0,'L');
+$pdf->Ln(5);
+$pdf->MultiCell(0,5,'PRESENT ORGANIZATION:',0,'L');
+$pdf->Ln(5);
+$pdf->MultiCell(0,5,'BASIC MONTHLY SALARY',0,'L');
+$pdf->Ln(5);
+$pdf->MultiCell(0,5,'SSS NUMBER:',0,'L');
+$pdf->Ln(10);
+$pdf->SetFont('Times');
+$pdf->MultiCell(0,5,'         This cetification is issued upon employee\'s request for SSS purposes.',0,'L');
+$pdf->Ln(30);   
+// $pdf->MultiCell(0,5,'Renelia L. Villanueva',0,'R');
+// $pdf->MultiCell(0,5,'Head',0,'R');
+// $pdf->MultiCell(0,5,'HRIS & Automation',0,'R');
+
+
+// eto yung last wag dugtungan. sa taas ka magdagdag.
+$pdf->SetFont('Times','B',12);
+$pdf->SetXY(100,107);
+$pdf->MultiCell(50,0,'J852',0,'L');
+$pdf->SetXY(100,117);
+$pdf->MultiCell(50,0,'Roberto Joaquin Zulueta',0,'L');
+$pdf->SetXY(100,127);
+$pdf->MultiCell(50,0,'January 4, 2019',0,'L');
+$pdf->SetXY(100,137);
+$pdf->MultiCell(70,0,'March 14, 1999(Manpower)',0,'L');
+$pdf->SetXY(100,147);
+$pdf->MultiCell(50,0,'Analyst-Programmer',0,'L');
+$pdf->SetXY(100,157);
+$pdf->MultiCell(70,0,'Application Development 3 Division',0,'L');
+$pdf->SetXY(100,167);
+$pdf->MultiCell(70,0,'Confidential',0,'L');
+$pdf->SetXY(100,177);
+$pdf->MultiCell(70,0,'3304511823',0,'L');
+
+$pdf->SetXY(95.5,37.5);
+$pdf->MultiCell(100,0,'Date:',0,'C');
+$pdf->SetXY(88,47.5);
+$pdf->MultiCell(100,0,'Ref:',0,'C');
+
+$pdf->SetFont('Times');
+$pdf->SetXY(100,108);
+$pdf->MultiCell(84,0,'_______________________________________________________________________________________',0,'L');
+$pdf->SetXY(100,118);
+$pdf->MultiCell(84,0,'_______________________________________________________________________________________',0,'L');
+$pdf->SetXY(100,128);
+$pdf->MultiCell(84,0,'_______________________________________________________________________________________',0,'L');
+$pdf->SetXY(100,138);
+$pdf->MultiCell(84,0,'_______________________________________________________________________________________',0,'L');
+$pdf->SetXY(100,148);
+$pdf->MultiCell(84,0,'_______________________________________________________________________________________',0,'L');
+$pdf->SetXY(100,158);
+$pdf->MultiCell(84,0,'_______________________________________________________________________________________',0,'L');
+$pdf->SetXY(100,168);
+$pdf->MultiCell(84,0,'_______________________________________________________________________________________',0,'L');
+$pdf->SetXY(100,178);
+$pdf->MultiCell(84,0,'_______________________________________________________________________________________',0,'L');
+
+// head
+$pdf->SetFont('Times');
+$pdf->SetXY(111,214);
+$pdf->MultiCell(100,0,'Renelia L. Villanueva',0,'C');
+$pdf->SetXY(111,216);
+$pdf->MultiCell(100,5,'Head',0,'C');
+$pdf->SetXY(111,220);
+$pdf->MultiCell(100,5,'HRIS & Automation',0,'C');
+
+$pdf->Output();
+?>
