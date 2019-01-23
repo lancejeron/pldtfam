9e42351c7c7c70b356262b8fa4badc612e1b96ba
robertojoaquin <joaqszxc@gmail.com>
2019-01-23 09:49:32 +0800

    Update navbar.php

--- production/template/navbar.php
+++ production/template/navbar.php
@@ -26,7 +26,7 @@
         <div class="menu_section">
         <h3>Menu</h3>
         <ul class="nav side-menu">
-            <li><a><i class="fa fa-edit"></i> Certificate<span class="fa fa-chevron-down"></span></a>
+            <li><a><i class="fa fa-edit"></i> Certification<span class="fa fa-chevron-down"></span></a>
             <ul class="nav child_menu">
                 <li><a href="index.php">Requests</a></li>
                 <li><a href="index2.php">Finished Requests</a></li>
